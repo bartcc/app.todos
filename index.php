2baae951aa5c8ce34f79802558f1537bef89e0e9
Axel Nitzschner <info@anito.de>
2011-07-30 21:41:44 +0200

    chg

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * master Requests collector.
+ * master changed Requests collector.
  *
  *  This file collects requests if:
  *	- no mod_rewrite is avilable or .htaccess files are not supported

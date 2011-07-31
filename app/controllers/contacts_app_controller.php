fb65674ab30baeca2a9b5a2edf5a1ffd33d19b3f
Axel Nitzschner <info@anito.de>
2011-07-31 11:38:46 +0200

    readme

--- app/controllers/contacts_app_controller.php
+++ app/controllers/contacts_app_controller.php
@@ -1,14 +1,17 @@
 <?php
+
 class ContactsAppController extends AppController {
 
-	var $name = 'ContactsApp';
-    var $uses = array();
-    
-    function beforeFilter() {
-        $this->autoRender = true;
-        $this->layout = 'contacts_app_layout';
-    }
-    
-    function index() {}
+  var $name = 'ContactsApp';
+  var $uses = array();
+
+  function beforeFilter() {
+    $this->autoRender = true;
+    $this->layout = 'contacts_layout';
+  }
+
+  function index() {}
+
 }
+
 ?>
--- app/controllers/todos_app_controller.php
+++ app/controllers/todos_app_controller.php
@@ -1,15 +1,19 @@
 <?php
+
 class TodosAppController extends AppController {
 
-	var $name = 'TodosApp';
-    var $uses = array();
+  var $name = 'TodosApp';
+  var $uses = array();
+
+  function beforeFilter() {
+    $this->autoRender = true;
+    $this->layout = 'todos_layout';
+  }
+
+  function index() {
     
-    function beforeFilter() {
-        $this->autoRender = true;
-        $this->layout = 'todos_layout';
-    }
-    
-    function index() {}
+  }
 
 }
+
 ?>

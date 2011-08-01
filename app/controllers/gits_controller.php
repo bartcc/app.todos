e833ebbe681ba7376bed371b0e481ac3a110fe45
Axel Nitzschner <info@anito.de>
2011-08-01 14:58:34 +0200

    x

--- app/controllers/gits_controller.php
+++ app/controllers/gits_controller.php
@@ -29,16 +29,18 @@ class GitsController extends AppController {
   function run_git($branch) {
     $this->log('SERVER_SOFTWARE:'.OS, LOG_DEBUG);
     if(OS !== NO_OS) {
+      
       if (OS === OS_MS) {
-        $p = 'Windows  ';
         $op = `"D:\Program Files\Git\bin\git.exe" checkout $branch 2>&1`;
       }
+      
       if (OS === OS_UX) {
-        $p = 'Mac OS X  ';
-        $op = `git checkout $branch 2>&1`;
+//        $op = `git checkout $branch 2>&1`;
+        $op = `git status 2>&1`;
       }
+      
       $this->log($_SERVER['SERVER_SOFTWARE'], LOG_DEBUG);
-      return ($op.'\r\n'.$p);
+      return $op;
     } else {
       die( );
     }

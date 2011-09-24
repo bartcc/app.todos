8e81cae936d042ee6f8dfd17c8e8ce069ffb7be6
Axel Nitzschner <info@anito.de>
2011-09-24 16:26:10 +0200

    core

--- app/config/core.php
+++ app/config/core.php
@@ -33,7 +33,7 @@
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-	Configure::write('debug', 0);
+	Configure::write('debug', 2);
 
 /**
  * CakePHP Log Level:
@@ -67,7 +67,7 @@
  * And uncomment the App.baseUrl below:
  */
 	//Configure::write('App.baseUrl', env('SCRIPT_NAME'));
-	Configure::write('App.baseUrl', env('SCRIPT_NAME') . '?');
+	//Configure::write('App.baseUrl', env('SCRIPT_NAME') . '?');
 
 /**
  * Uncomment the define below to use CakePHP prefix routes.

5593912288b671499cc4df0109e7d456e607b3a0
Axel Nitzschner <info@anito.de>
2011-10-04 11:05:37 +0200

    continued

--- app/models/gallery.php
+++ app/models/gallery.php
@@ -37,18 +37,6 @@ class Gallery extends AppModel {
       )
   );
 
-  function afterFind($results, $primary) {
-    
-//    foreach ($results as $key => $val) {
-//      $results[$key] = $val[$this->alias];
-//    }
-    return $results;
-    
-//    if($primary == true && !is_object($results)) {
-//        return Set::map($results);
-//    }
-  }
-
 }
 
 ?>

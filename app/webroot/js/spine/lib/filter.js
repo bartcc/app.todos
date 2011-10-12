f24ce1dfaa0d34dd9d0cf0c643d393398c070780
Axel Nitzschner <info@anito.de>
2011-10-13 01:58:38 +0200

    continued

--- app/webroot/js/spine/lib/filter.js
+++ app/webroot/js/spine/lib/filter.js
@@ -1,39 +1,41 @@
-var Filter;
-Filter = function() {
-  var extend, include;
-  extend = {
-    filter: function(query, func) {
-      if (func == null) {
-        func = 'select';
-      }
-      if (!query) {
-        return this.all();
-      }
-      return this.select(function(item) {
-        return item[func](query);
-      });
-    }
-  };
-  include = {
-    select: function(query) {
-      var atts, key, value;
-      query = query.toLowerCase();
-      atts = (this.selectAttributes || this.attributes).apply(this);
-      for (key in atts) {
-        value = atts[key];
-        value = value.toLowerCase();
-        if (!((value != null ? value.indexOf(query) : void 0) === -1)) {
-          return true;
+(function() {
+  var Filter;
+  Filter = function() {
+    var extend, include;
+    extend = {
+      filter: function(query, func) {
+        if (func == null) {
+          func = 'select';
         }
+        if (!query) {
+          return this.all();
+        }
+        return this.select(function(item) {
+          return item[func](query);
+        });
       }
-      return false;
-    }
+    };
+    include = {
+      select: function(query) {
+        var atts, key, value;
+        query = query.toLowerCase();
+        atts = (this.selectAttributes || this.attributes).apply(this);
+        for (key in atts) {
+          value = atts[key];
+          value = value.toLowerCase();
+          if (!((value != null ? value.indexOf(query) : void 0) === -1)) {
+            return true;
+          }
+        }
+        return false;
+      }
+    };
+    return {
+      extended: function() {
+        this.extend(extend);
+        return this.include(include);
+      }
+    };
   };
-  return {
-    extended: function() {
-      this.extend(extend);
-      return this.include(include);
-    }
-  };
-};
-Spine.Model.Filter = Filter();
+  Spine.Model.Filter = Filter();
+}).call(this);

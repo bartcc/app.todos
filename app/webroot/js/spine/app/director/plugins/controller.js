73e47265da34033626a405e0cdb231ccabc8182c
Axel Nitzschner <info@anito.de>
2011-10-15 01:50:39 +0200

    continued

--- app/webroot/js/spine/app/director/plugins/controller.js
+++ app/webroot/js/spine/app/director/plugins/controller.js
@@ -1,33 +1,31 @@
-(function() {
-  var $;
-    if (typeof Spine !== "undefined" && Spine !== null) {
-    Spine;
-  } else {
-    Spine = require("spine");
-  };
-  $ = Spine.$;
-  Spine.Controller.include({
-    focusFirstInput: function(el) {
-      if (!el) {
-        return;
-      }
-      if (el.is(':visible')) {
-        $('input', el).first().focus().select();
-      }
-      return el;
-    },
-    openPanel: function(controller, target) {
-      App[controller].deactivate();
-      return target.click();
-    },
-    isCtrlClick: function(e) {
-      return e.metaKey || e.ctrlKey || e.altKey;
+var $;
+if (typeof Spine !== "undefined" && Spine !== null) {
+  Spine;
+} else {
+  Spine = require("spine");
+};
+$ = Spine.$;
+Spine.Controller.include({
+  focusFirstInput: function(el) {
+    if (!el) {
+      return;
     }
-  });
-  Spine.Controller.extend({
-    empty: function() {
-      console.log('empty');
-      return this.constructor.apply(this, arguments);
+    if (el.is(':visible')) {
+      $('input', el).first().focus().select();
     }
-  });
-}).call(this);
+    return el;
+  },
+  openPanel: function(controller, target) {
+    App[controller].deactivate();
+    return target.click();
+  },
+  isCtrlClick: function(e) {
+    return e.metaKey || e.ctrlKey || e.altKey;
+  }
+});
+Spine.Controller.extend({
+  empty: function() {
+    console.log('empty');
+    return this.constructor.apply(this, arguments);
+  }
+});

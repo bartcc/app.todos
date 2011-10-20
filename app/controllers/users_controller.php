1025ec2f79dc85a4b5297e704be2d49dfdbab8ef
Axel Nitzschner <info@anito.de>
2011-10-20 14:11:43 +0200

    continued

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -1,21 +1,26 @@
 <?php
+
 class UsersController extends AppController {
 
-	var $name = 'Users';
+  var $name = 'Users';
 
- function beforeFilter() {
+  function beforeFilter() {
     $this->disableCache();
     $this->Auth->allowedActions = array('login', 'logout', 'auth');
 
     $this->Cookie->name = 'TODOS';
     $this->Cookie->time = 3600; // 1 hour
 
+    $this->User->Group->recursive = 0;
+    $group = $this->User->Group->findById($this->Auth->user('group_id'));
+    $this->groupname = $group['Group']['name'];
+    
     parent::beforeFilter();
   }
- 
- function login() {
+
+  function login() {
     $user = $this->Auth->user();
-    if($this->RequestHandler->isAjax()) {
+    if ($this->RequestHandler->isAjax()) {
       if ($user) {
         $this->User->Group->recursive = 0;
         $group = $this->User->Group->findById($this->Auth->user('group_id'));
@@ -60,32 +65,40 @@ class UsersController extends AppController {
     $this->render(SIMPLE_JSON);
   }
 
- 
-	function index() {
-		$this->User->recursive = 0;
-		$this->set('users', $this->paginate());
-	}
+  function index() {
+    if ($this->groupname != 'Administrators') {
+      $this->redirect(array('action' => 'login'));
+    }
+    $this->User->recursive = 0;
+    $this->set('users', $this->paginate());
+  }
 
-	function view($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid user', true));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->set('user', $this->User->read(null, $id));
-	}
- 
- function add() {
+  function view($id = null) {
+    if ($this->groupname != 'Administrators') {
+      $this->redirect(array('action' => 'login'));
+    }
+    if (!$id) {
+      $this->Session->setFlash(__('Invalid user', true));
+      $this->redirect(array('action' => 'index'));
+    }
+    $this->set('user', $this->User->read(null, $id));
+  }
+
+  function add() {
+    if ($this->groupname != 'Administrators') {
+      $this->redirect(array('action' => 'login'));
+    }
     if (!empty($this->data)) {
       $this->User->create();
       if ($this->User->save($this->data)) {
         $this->Session->setFlash(__('The user has been saved', true));
-        if($this->RequestHandler->isAjax()) {
+        if ($this->RequestHandler->isAjax()) {
           $this->render(SIMPLE_JSON);
         } else {
           $this->redirect(array('action' => 'index'));
         }
       } else {
-        if($this->RequestHandler->isAjax()) {
+        if ($this->RequestHandler->isAjax()) {
           $value = $this->User->invalidFields();
           foreach ($value as $field => $error) {
             $json = array_merge($value, array($field => array('error' => $error)));
@@ -102,37 +115,45 @@ class UsersController extends AppController {
     $this->set(compact('groups'));
   }
 
-	function edit($id = null) {
-		if (!$id && empty($this->data)) {
-			$this->Session->setFlash(__('Invalid user', true));
-			$this->redirect(array('action' => 'index'));
-		}
-		if (!empty($this->data)) {
-			if ($this->User->save($this->data)) {
-				$this->Session->setFlash(__('The user has been saved', true));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The user could not be saved. Please, try again.', true));
-			}
-		}
-		if (empty($this->data)) {
-			$this->data = $this->User->read(null, $id);
-		}
-		$groups = $this->User->Group->find('list');
-		$this->set(compact('groups'));
-	}
+  function edit($id = null) {
+    if ($this->groupname != 'Administrators') {
+      $this->redirect(array('action' => 'login'));
+    }
+    if (!$id && empty($this->data)) {
+      $this->Session->setFlash(__('Invalid user', true));
+      $this->redirect(array('action' => 'index'));
+    }
+    if (!empty($this->data)) {
+      if ($this->User->save($this->data)) {
+        $this->Session->setFlash(__('The user has been saved', true));
+        $this->redirect(array('action' => 'index'));
+      } else {
+        $this->Session->setFlash(__('The user could not be saved. Please, try again.', true));
+      }
+    }
+    if (empty($this->data)) {
+      $this->data = $this->User->read(null, $id);
+    }
+    $groups = $this->User->Group->find('list');
+    $this->set(compact('groups'));
+  }
 
-	function delete($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid id for user', true));
-			$this->redirect(array('action'=>'index'));
-		}
-		if ($this->User->delete($id)) {
-			$this->Session->setFlash(__('User deleted', true));
-			$this->redirect(array('action'=>'index'));
-		}
-		$this->Session->setFlash(__('User was not deleted', true));
-		$this->redirect(array('action' => 'index'));
-	}
+  function delete($id = null) {
+    if ($this->groupname != 'Administrators') {
+      $this->redirect(array('action' => 'login'));
+    }
+    if (!$id) {
+      $this->Session->setFlash(__('Invalid id for user', true));
+      $this->redirect(array('action' => 'index'));
+    }
+    if ($this->User->delete($id)) {
+      $this->Session->setFlash(__('User deleted', true));
+      $this->redirect(array('action' => 'index'));
+    }
+    $this->Session->setFlash(__('User was not deleted', true));
+    $this->redirect(array('action' => 'index'));
+  }
+
 }
+
 ?>

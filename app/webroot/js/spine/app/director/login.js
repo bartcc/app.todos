eee97c0d3d813f896fce3d95fa81c13bbc2bb1a7
Axel Nitzschner <info@anito.de>
2012-01-22 15:47:51 +0100

    changed

--- app/webroot/js/spine/app/director/login.js
+++ app/webroot/js/spine/app/director/login.js
@@ -69,7 +69,7 @@ Login = (function() {
     user.save();
     this.render(this.flashEl, this.flashTemplate, json);
     delayedFunc = function() {
-      return User.redirect('/');
+      return User.redirect('');
     };
     return this.delay(delayedFunc, 500);
   };

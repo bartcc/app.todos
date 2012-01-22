075873cb5dfabf42d7f67e4d0f66cc837a6d7a01
Axel Nitzschner <info@anito.de>
2012-01-22 11:42:22 +0100

    changed

--- app/config/routes.php
+++ app/config/routes.php
@@ -27,7 +27,8 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/views/pages/home.ctp)...
  */
-Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+Router::connect('/', array('controller' => 'director_app'));
+//Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
 Router::connectNamed(array('passed'));
 Router::connect('/q/*', array('controller' => 'kodaks', 'action' => 'develop'));
 Router::connect('/git/*', array('controller' => 'gits', 'action' => 'exec'));

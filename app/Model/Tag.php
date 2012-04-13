8b6d108ddadca384263dc99a5c22218a695e7ecf
Axel Nitzschner <info@anito.de>
2012-04-13 21:33:45 +0200

    deleted old version

--- app/Model/Tag.php
+++ app/Model/Tag.php
@@ -3,16 +3,14 @@
 App::uses('AppModel', 'Model');
 
 class Tag extends AppModel {
+	public $name = 'Tag';
+	public $useDbConfig = 'cakephp_spine';
+	public $displayField = 'name';
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
 
-  public $name = 'Tag';
-  public $displayField = 'name';
-  public $useDbConfig = 'director_spine';
-  //The Associations below have been created with all possible keys, those that are not needed can be removed
-
-  public $hasMany = array(
-      'PhotosTag'
-  );
+	public $hasMany = array(
+		'PhotosTag'
+	);
 
 }
-
 ?>

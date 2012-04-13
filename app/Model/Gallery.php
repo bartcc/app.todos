aa8bf2b69bbdb7f416a6f63f5c779354c5a48276
Axel Nitzschner <info@anito.de>
2012-04-13 21:33:39 +0200

    deleted old version

--- app/Model/Gallery.php
+++ app/Model/Gallery.php
@@ -6,30 +6,10 @@ class Gallery extends AppModel {
 
   public $name = 'Gallery';
   public $displayField = 'name';
-  public $useDbConfig = 'director_spine';
   
   public $hasMany = array(
       'GalleriesAlbum'
   );
-
-// The Associations below have been created with all possible keys, those that are not needed can be removed
-//	var $hasAndBelongsToMany = array(
-//		'Album' => array(
-//			'className' => 'Album',
-//			'joinTable' => 'galleries_albums',
-//			'foreignKey' => 'gallery_id',
-//			'associationForeignKey' => 'album_id',
-//			'unique' => true,
-//			'conditions' => '',
-//			'fields' => '',
-//			'order' => '',
-//			'limit' => '',
-//			'offset' => '',
-//			'finderQuery' => '',
-//			'deleteQuery' => '',
-//			'insertQuery' => ''
-//		)
-//	);
 }
 
 ?>

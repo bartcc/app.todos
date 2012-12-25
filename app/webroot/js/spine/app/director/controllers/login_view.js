84da6a07d8786ad4272b7218b142a797da7c18a2
Axel Nitzschner <info@anito.de>
2012-12-25 23:11:08 +0100

    GitHub for Mac: Throw-away commit.

---
+++ app/webroot/js/spine/app/director/controllers/login_view.js
@@ -0,0 +1,33 @@
+var LoginView;
+var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
+  for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
+  function ctor() { this.constructor = child; }
+  ctor.prototype = parent.prototype;
+  child.prototype = new ctor;
+  child.__super__ = parent.prototype;
+  return child;
+};
+LoginView = (function() {
+  __extends(LoginView, Spine.Controller);
+  LoginView.prototype.elements = {
+    'button': 'logoutEl'
+  };
+  LoginView.prototype.events = {
+    'click button': 'logout'
+  };
+  function LoginView() {
+    LoginView.__super__.constructor.apply(this, arguments);
+  }
+  LoginView.prototype.template = function(item) {
+    return $('#loggedinTemplate').tmpl(item);
+  };
+  LoginView.prototype.logout = function() {
+    console.log('click');
+    localStorage.hash = location.hash;
+    return User.redirect('logout');
+  };
+  LoginView.prototype.render = function(item) {
+    return this.html(this.template(item));
+  };
+  return LoginView;
+})();

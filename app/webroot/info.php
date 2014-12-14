a8c14372743b791b936e477a9492ec039ac7b49c
Axel Nitzschner <info@anito.de>
2014-12-14 17:01:42 +0100

    Revert "soap tests"
    This reverts commit 28df030ce8e656e934ff74834e900d0dd09dee4d.

--- app/webroot/info.php
+++
@@ -1,6 +0,0 @@
-<?php
-// You can use Server Admin to enable the Apache PHP module; it's disabled by default.
-// You can uncomment the phpinfo() directive below to provide a default PHP info page
-// but note that this displays information about your host's configuration.
-phpinfo();
-?>
